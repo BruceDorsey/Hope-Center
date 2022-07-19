f7a9cd405c3b41a50b46d52edc2338a01310e3ce
BruceDorsey <bruce.dorsey@cdpipelinedevshop.com>
2022-07-19 18:54:40 +0000

    “Hope-Center

---
+++ hopecenter/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '/workspace/Hope-Center/hopecenter/src/App.css';
+import { SidebarData } from "./SidebarData";
+
+
+function Sidebar(){
+
+    return(
+        <div className='Sidebar' ></div>
+    )
+}
+export default Sidebar;
---
+++ hopecenter/src/components/SidebarData.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function SidebarList(){
+
+    return(
+        <button></button>
+    )
+}
+export default SidebarList
